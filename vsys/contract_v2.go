99d1bfa13746ca2c05d5724c3cf439750387842f
joy <joy060102@gmail.com>
2021-11-30 15:28:43 +0800

    changes to const

--- vsys/contract_v2.go
+++ vsys/contract_v2.go
@@ -1,5 +1,20 @@
 package vsys
 
+const (
+	Funcidx2Supersede    = 0
+	Funcidx2Issue	     = 1
+	Funcidx2Destroy      = 2
+	Funcidx2UpdateList   = 3
+	Funcidx2Send     	 = 4
+	Funcidx2Transfer     = 5
+	Funcidx2Deposit      = 6
+	Funcidx2Withdraw   	 = 7
+	Funcidx2TotalSupply  = 8
+	Funcidx2MaxSupply    = 9
+	Funcidx2BalanceOf    = 10
+	Funcidx2GetIssuer    = 11
+)
+
 type ContractV2 struct {
 	Address string
 	AddToList 	bool
